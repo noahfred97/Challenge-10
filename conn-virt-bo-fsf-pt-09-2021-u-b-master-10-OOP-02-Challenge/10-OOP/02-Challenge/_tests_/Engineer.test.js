fef961aac1915207d4a696d377d436ebae4a6d08
noahfred97 <90789790+noahfred97@users.noreply.github.com>
2022-03-09 22:39:08 -0500

    finalfix

--- conn-virt-bo-fsf-pt-09-2021-u-b-master-10-OOP-02-Challenge/10-OOP/02-Challenge/_tests_/Engineer.test.js
+++ conn-virt-bo-fsf-pt-09-2021-u-b-master-10-OOP-02-Challenge/10-OOP/02-Challenge/_tests_/Engineer.test.js
@@ -1,7 +1,7 @@
 const Engineer = require('../lib/Engineer.js');
 
 test('creates an Engineer object', () => {
-    const engineer = new Engineer('Noah2', 2, "email@email.email", 2585485);
+    const engineer = new Engineer('Noah2', 2, "noahfred97@gmail.com", 555555);
   
     expect(engineer.name).toEqual(expect.any(String));
     expect(engineer.empID).toEqual(expect.any(Number));
